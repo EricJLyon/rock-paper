9a54c2a66ab7265f0f10fb08e89425b944d71af9
Eric Lyon <ericjlyon@gmail.com>
2022-02-18 12:09:06 -0800

    finished according to stated parameters on assig nment; not ideal, may fix up later.

--- scripts.js
+++ scripts.js
@@ -11,30 +11,73 @@ function computerPlay() {
     }
 }
 
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
 function playRound(playerSelection, computerSelection) {
     let playerCased = playerSelection.toLowerCase();
     let computerCased = computerSelection.toLowerCase();
+    let p = document.createElement('p');
     if (playerCased === computerCased) {
-        console.log(`It's a tie! You both picked ${computerSelection}`);
-        return('tie');
+        let result = document.createTextNode(`It's a tie! You both picked ${computerSelection}`);
+        p.appendChild(result);
+        document.body.appendChild(p);
+        ties += 1;
+        tieTotal.innerHTML = `Ties: ${ties}`;
     } else if ((playerCased === 'rock') && (computerCased === 'scissors')) {
-        console.log("You win! Rock crushes Scissors.");
-        return('win');
+        let result = document.createTextNode("You win! Rock crushes Scissors.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        wins += 1;
+        winTotal.innerHTML = `Wins: ${wins}`;
     } else if ((playerCased === 'scissors') && (computerCased === 'paper')) {
-        console.log("You win! Scissors cut Paper.");
-        return('win');
+        let result = document.createTextNode("You win! Scissors cut Paper.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        wins += 1;
+        winTotal.innerHTML = `Wins: ${wins}`;
     } else if ((playerCased === 'paper') && (computerCased === 'rock')) {
-        console.log("You win! Paper covers Rock.");
-        return('win');
+        let result = document.createTextNode("You win! Paper covers Rock.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        wins += 1;
+        winTotal.innerHTML = `Wins: ${wins}`;
     } else if ((playerCased === 'scissors') && (computerCased === 'rock')) {
-        console.log("You lose! Rock crushes Scissors.");
-        return('lose');
+        let result = document.createTextNode("You lose! Rock crushes Scissors.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        losses += 1;
+        loseTotal.innerHTML = `Losses: ${losses}`;
     } else if ((playerCased === 'paper') && (computerCased === 'scissors')) {
-        console.log("You lose! Scissors cut Paper.");
-        return('lose');
+        let result = document.createTextNode("You lose! Scissors cut Paper.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        losses += 1;
+        loseTotal.innerHTML = `Losses: ${losses}`;
     } else if ((playerCased === 'rock') && (computerCased === 'paper')) {
-        console.log("You lose! Paper covers Rock.");
-        return('lose');
+        let result = document.createTextNode("You lose! Paper covers Rock.");
+        p.appendChild(result);
+        document.body.appendChild(p);
+        losses += 1;
+        loseTotal.innerHTML = `Losses: ${losses}`;
+    }
+    if ((wins === 5) || (losses === 5)) {
+        let p = document.createElement('p');
+        if (wins === 5) {
+            let result = document.createTextNode(`You win! Congratulations!`);
+            p.appendChild(result);
+            document.body.appendChild(p);
+        }
+        if (losses === 5) {
+            let result = document.createTextNode(`The computer wins! Better luck next time.`);
+            p.appendChild(result);
+            document.body.appendChild(p);
+        }
+        rock.removeEventListener('click', () => {playRound('Rock',computerPlay());});
+        paper.removeEventListener('click', () => {playRound('Paper',computerPlay());});
+        scissors.removeEventListener('click', () => {playRound('Scissors',computerPlay());});
+    
     }
 }
 
@@ -67,3 +110,8 @@ function game() {
 
     console.log(`A winner is ${winner}! Your record is ${playerWins}-${computerWins}-${ties}.`)
 }
+
+rock.addEventListener('click', () => {playRound('Rock',computerPlay());});
+paper.addEventListener('click', () => {playRound('Paper',computerPlay());});
+scissors.addEventListener('click', () => {playRound('Scissors',computerPlay());});
+
